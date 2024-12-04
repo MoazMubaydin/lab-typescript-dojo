46e1ab2c5ed7b8d5415a2e8d39bc3d53d6669ce9
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-05 00:01:38 +0100

    iteration 2

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -2,4 +2,22 @@
 // Iteration 2 | Functions
 //
 
+function calcMultiplication(a:number,b:number):number{
+    return a * b;
+}
 
+function isEven(n:number):boolean{
+    return n%2 ===0 
+}
+
+function calcArrayAverage(numbersArr:Array<number>):string | number{
+    if(numbersArr.length === 0 ){
+        return "Please provide an array with at least one element"
+    }
+    let total = 0;
+
+    numbersArr.forEach((num:number)=>{
+        total+=num
+    })
+    return total/numbersArr.length
+}
