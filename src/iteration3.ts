2c9a0825470f36cf653bc31631ae662760b68cc7
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-05 00:13:08 +0100

    iteartion 3

--- src/iteration3.ts
+++ src/iteration3.ts
@@ -3,3 +3,33 @@
 //
 
 
+class BankAccount {
+    balance:number= 0;
+    accountHolder:string;
+
+    constructor(accountHolder:string){
+        this.accountHolder=accountHolder;
+    }
+
+    getBalance():number{
+        return this.balance;
+    }
+    
+    deposit(amount:number):number | string{
+        if(amount <= 0){
+            return "please provide a valid amout";
+        }
+        return this.balance+=amount;
+
+    }
+    withdraw(amount:number){
+        if(amount <= 0){
+            "please provid a valid amout"
+        }
+        if(amount > this.balance){
+            return "Insufficient funds"
+        }
+       return this.balance -= amount
+    }
+    
+}
