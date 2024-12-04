e781a5eb2cbb005584122d5059b0e206bb50561a
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-05 00:27:10 +0100

    iteartion 4

--- src/iteration4.ts
+++ src/iteration4.ts
@@ -2,4 +2,41 @@
 // Iteration 4 | Type aliases
 //
 
+type Book ={
+    title:string,
+    year:number,
+    genres:string[],
+    rating?:number,
+    auther:string,
+    numberOfPages?:number
+}
 
+type Movie ={
+    title:string,
+    year:number,
+    genres:string[],
+    rating?:number,
+    director:string,
+    durationInMinutes?:number
+}
+/*enum MusicAlbum {
+    "studio Album",
+    "Live Album",
+    "Soundtrack",
+    "Other"
+}*/
+type Music ={
+    title:string;
+    year:number;
+    genres:string[];
+    rating?:number;
+    artist:string;
+    numberOfTracks?:number;
+    durationInMinutes:number;
+    MusicAlbum: "studio Album"| "Live Album"|"Soundtrack"|"Other";
+  }
+
+  function getYearsSinceRelease(mediaItem:Book | Movie | Music):number{
+    const currentYear = new Date().getFullYear();
+    return currentYear - mediaItem.year
+  }
